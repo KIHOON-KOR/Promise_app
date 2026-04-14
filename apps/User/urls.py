b35f2e5a683989574f7e0eec6db3edae57698dd6
kihoon <nike6736@naver.com>
2026-04-14 22:41:56 +0900

    ✨ feat(#15): 회원탈퇴 기능 구현

--- apps/User/urls.py
+++ apps/User/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from apps.User.views.login_view import UserLoginView
 from apps.User.views.logout_view import UserLogoutView
 from apps.User.views.signup_view import UserSignupView
+from apps.User.views.withdrawal_view import UserWithdrawalView
 
 urlpatterns = [
     path("signup/", UserSignupView.as_view(), name="signup"),
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", UserLogoutView.as_view(), name="logout"),
+    path('withdrawal/', UserWithdrawalView.as_view(), name='withdrawal'),
 ]
