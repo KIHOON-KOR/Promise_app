0f65f2d76dab91023a181e2c78b239ef21953b9f
kihoon <nike6736@naver.com>
2026-04-15 18:52:54 +0900

    ✨ feat(#22): 권한 부여 기능 구현

--- apps/Event/urls.py
+++ apps/Event/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from apps.Event.views.event_create_view import EventCreateView
 from apps.Event.views.event_invite_view import EventInviteView
+from apps.Event.views.event_permission_view import EventGrantPermissionView
 
 urlpatterns = [
     # 약속을 생성하는 주소
     path("", EventCreateView.as_view(), name="event_create"),
     # 특정 약속에 누군가를 초대하는 주소
     path("<int:event_id>/invite/", EventInviteView.as_view(), name="event_invite"),
+    # 특정 약속에서 누군가에게 초대 권한을 주는 주소
+    path("<int:event_id>/grant/", EventGrantPermissionView.as_view(), name="event_grant_permission"),
 ]
